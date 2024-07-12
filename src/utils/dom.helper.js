f5d337ed3f3d2542547f9c84ec65023fc8e4e68e
Jose Jimenez <ojesworks@gmail.com>
2024-07-12 15:13:46 -0300

    normalize attr naby by snake formmat

--- src/utils/dom.helper.js
+++ src/utils/dom.helper.js
@@ -1,8 +1,9 @@
+const snake = (value) => value.replace(/([A-Z])/g, (_, v) => `-${v.toLowerCase()}`);
 export const createTag = (tagName, attributes) => {
   const element = document.createElement(tagName);
 
   for (let attrName in attributes) {
-    element.setAttribute(attrName, attributes[attrName]);
+    element.setAttribute(snake(attrName), attributes[attrName]);
   }
 
   return element;
